35ba4390a9f3fa1d0d34be2c0288ab0b17bc3c49
Nirvighna Dev <dev@nirvighna.org>
2026-08-22 09:50:01 +0530

    Eliminate initial black screen with Android 12+ splash theme, ivory native webview, pre-mount DOM splash, and regenerated ivory drawables

--- android/app/src/main/java/org/nirvighna/pilgrim/MainActivity.java
+++ android/app/src/main/java/org/nirvighna/pilgrim/MainActivity.java
@@ -1,15 +1,25 @@
 package org.nirvighna.pilgrim;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Force native window and decor background to warm ivory #FAF7F2 from frame 0
+        if (getWindow() != null && getWindow().getDecorView() != null) {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#FAF7F2"));
+        }
+
         if (this.bridge != null && this.bridge.getWebView() != null) {
-            WebSettings settings = this.bridge.getWebView().getSettings();
+            WebView webView = this.bridge.getWebView();
+            webView.setBackgroundColor(Color.parseColor("#FAF7F2"));
+            WebSettings settings = webView.getSettings();
             settings.setMediaPlaybackRequiresUserGesture(false);
         }
     }
